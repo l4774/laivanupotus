9caba77704a8e8dbc983b3a2ab00b6d90591cf69
Johannes Pennanen <l4774@student.jamk.fi>
2017-11-28 13:52:51 +0200

    1

---
+++ Laivanupotus/laivanupotus/Source.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_TAULU = 4;
+
+int main()
+{
+	int taulu[MAX_TAULU] = { 2,7,1,3 };
+	int i = 0;
+	int j = 0;
+
+	cout << "Alkuperainen" << endl;
+	for (i = 0; i < MAX_TAULU; i++)
+		cout << taulu[i] << " ";
+	cout << endl;
+
+
+	for (i = 0; i < (MAX_TAULU - 1); i++)
+		for (j = i + 1; j < MAX_TAULU; j++)
+			if (taulu[j]<taulu[i])
+			{
+				int apu = taulu[i];
+				taulu[i] = taulu[j];
+				taulu[j] = apu;
+			}
+
+	cout << "Jarjestetty" << endl;
+	for (i = 0; i < MAX_TAULU; i++)
+		cout << taulu[i] << " ";
+	cout << endl;
+
+	return 0;
+}
